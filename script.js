b1312e13db54c7a85aac85c321eb19e9bc0b31a4
Tobi_03 <m4sk3d.t0bi@gmail.com>
2025-12-30 23:25:42 +0530

    made changes in working

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
-let millisecond = 0;
+// let millisecond = 0;
+
+// function countdown()
+// {
+//     millisecond = millisecond + 0.1;
+//     console.log(`count: ${millisecond}`);
+
+//     if (millisecond >= 5)
+//     {
+//         clearInterval(intervals);
+//     }
+// }
+
+// const intervals = setInterval(countdown, 100);
+
+let hour = 0;
+let minute = 0;
+let second = 0;
 
 function countdown()
 {
-    millisecond = millisecond + 0.1;
-    console.log(`count: ${millisecond}`);
-
-    if (millisecond >= 5)
+    second++;
+    if (second == 60)
     {
-        clearInterval(intervals);
+        minute++;
+        second = 0;
     }
+    if (minute == 60)
+    {
+        hour++;
+        minute = 0;
+    }
+    console.log(`the time is ${hour}:${minute}:${second}`)
 }
 
-const intervals = setInterval(countdown, 100);
-
+const timeinterval = setInterval(countdown, 1000);
