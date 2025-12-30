be43c38f0265379c12c110f7b09976d8cac81514
Tobi_03 <m4sk3d.t0bi@gmail.com>
2025-12-30 23:33:12 +0530

    made changes

--- script.js
+++ script.js
@@ -1,36 +1,30 @@
-// let millisecond = 0;
+const hour = document.getElementById('hour');
+const minute = document.getElementById('minute');
+const second = document.getElementById('second');
+const startbutton =  document.getElementById('start');
+const stopbutton =
 
-// function countdown()
-// {
-//     millisecond = millisecond + 0.1;
-//     console.log(`count: ${millisecond}`);
-
-//     if (millisecond >= 5)
-//     {
-//         clearInterval(intervals);
-//     }
-// }
-
-// const intervals = setInterval(countdown, 100);
-
-let hour = 0;
-let minute = 0;
-let second = 0;
+// let hour = 0;
+// let minute = 0;
+// let second = 0;
 
 function countdown()
 {
-    second++;
+    second.textContent++;
     if (second == 60)
     {
-        minute++;
-        second = 0;
+        second.textContent = 0;
+        minute.textContent++;
     }
     if (minute == 60)
-    {
-        hour++;
-        minute = 0;
+        {
+        second.textContent = 0;
+        minute.textContent = 0;
+        hour.textContent += 1;
     }
-    console.log(`the time is ${hour}:${minute}:${second}`)
+
+    const stopbutton = addEventListener('click' )
+    // console.log(`the time is ${hour}:${minute}:${second}`)
 }
 
 const timeinterval = setInterval(countdown, 1000);
