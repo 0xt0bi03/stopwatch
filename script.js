0fe9c1f82ee806eae7fe64c5dd399409d0a96926
Tobi_03 <m4sk3d.t0bi@gmail.com>
2025-12-30 23:58:06 +0530

    timer fixed

--- script.js
+++ script.js
@@ -3,37 +3,48 @@ const minute = document.getElementById('minute');
 const second = document.getElementById('second');
 const startbutton =  document.getElementById('start');
 const stopbutton = document.getElementById('stop');
+const resetbutton = document.getElementById('reset');
 
 
 
-function countdown()
+function showtime()
 {
-    second.textContent++;
-    if (second == 60)
-        {
-            second.textContent = 0;
-            minute.textContent++;
-    }
-    if (minute == 60)
-        {
-            second.textContent = 0;
-            minute.textContent = 0;
-            hour.textContent += 1;
-        }
-        
-        const stopbutton = addEventListener('click', () =>
-            {
-                clearInterval(timeinterval);
-                
-            }
-        )
-    }
+    let hr = Number(hour.textContent);
+    let min = Number(minute.textContent);
+    let sec = Number(second.textContent);
     
-startbutton.addEventListener('click', () =>
+    sec++;
+    if (sec == 60)
+    {
+        sec = 0;
+        min++;
+    }
+    if (min == 60)
+    {
+        hr++;
+        min = 0;
+        sec = 0;
+    }
+    hour.textContent = hr;
+    minute.textContent = min;
+    second.textContent = sec;
+}
+
+let timeinterval;
+
+startbutton.addEventListener('click', () => 
 {
-    const timeinterval = setInterval(countdown, 1000);
+    timeinterval = setInterval(showtime, 1000);
 })
 
-second.textContent = 0;
-minute.textContent = 0;
-hour.textContent = 0;
+stopbutton.addEventListener('click', () =>
+    {
+        clearInterval(timeinterval);
+    })
+    
+resetbutton.addEventListener('click', () =>
+{
+    hour.textContent = 0;
+    minute.textContent = 0;
+    second.textContent = 0;
+})
