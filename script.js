f6f9b840eff8290c5d412dfd717066662cfd4c0f
Tobi_03 <m4sk3d.t0bi@gmail.com>
2025-12-30 23:41:32 +0530

    made changes

--- script.js
+++ script.js
@@ -6,31 +6,34 @@ const stopbutton = document.getElementById('stop');
 
 
 
-
 function countdown()
 {
     second.textContent++;
     if (second == 60)
-    {
-        second.textContent = 0;
-        minute.textContent++;
+        {
+            second.textContent = 0;
+            minute.textContent++;
     }
     if (minute == 60)
         {
-        second.textContent = 0;
-        minute.textContent = 0;
-        hour.textContent += 1;
+            second.textContent = 0;
+            minute.textContent = 0;
+            hour.textContent += 1;
+        }
+        
+        const stopbutton = addEventListener('click', () =>
+            {
+                clearInterval(timeinterval);
+                
+            }
+        )
     }
     
-    const stopbutton = addEventListener('click', () =>
-    {
-        second.textContent = 0;
-        minute.textContent = 0;
-        hour.textContent = 0;
-        clearInterval(timeinterval);
-            
-    }
-    )
-}
+startbutton.addEventListener('click', () =>
+{
+    const timeinterval = setInterval(countdown, 1000);
+})
 
-const timeinterval = setInterval(countdown, 1000);
+second.textContent = 0;
+minute.textContent = 0;
+hour.textContent = 0;
